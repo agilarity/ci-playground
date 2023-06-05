5b3234cf25e310f76cd6a77815bf053e7ab7ad2a
Joseph Cruz <agilarity@gmail.com>
2023-06-04 21:55:57 -0400

    test: inline simulated action

--- simulate-action.sh
+++ simulate-action.sh
@@ -9,18 +9,6 @@ else
     echo Verifying with test data
 fi
 
-example_file_directories=$(
-    for file in $files; do
-        echo $file | grep -o1 -Ei "^examples\/.*\/"
-    done
-)
-
-declare -a example_directories=($example_file_directories)
-declare -A unique_example_directories
-for dir in "${example_directories[@]}"; do
-    unique_example_directories[$dir]=0
-done
-
 working_directory=$(pwd)
 echo Simulate actions from $(pwd)
 for dir in $files; do
